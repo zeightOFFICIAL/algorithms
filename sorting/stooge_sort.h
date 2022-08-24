ec48be57e23d2db578143bdc01210b7108bd8c40
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-08-25 00:56:28 +0300

    stooge sort fixing

--- sorting/stooge_sort.h
+++ sorting/stooge_sort.h
@@ -1,11 +1,11 @@
 // stooge_sort.h
 
 /*
-Stooge Sort is a recursive sorting algorithm. It is not much 
-efficient but interesting sorting algorithm. It generally 
-divides the array into two overlapping parts (2/3 each). After 
-that it performs sorting in first 2/3 part and then it performs 
-sorting in last 2/3 part. And then, sorting is done on first 2/3 
+Stooge Sort is a recursive sorting algorithm. It is not much
+efficient but interesting sorting algorithm. It generally
+divides the array into two overlapping parts (2/3 each). After
+that it performs sorting in first 2/3 part and then it performs
+sorting in last 2/3 part. And then, sorting is done on first 2/3
 part to ensure that the array is sorted.
 https://www.geeksforgeeks.org/stooge-sort/
 */
@@ -13,32 +13,47 @@ https://www.geeksforgeeks.org/stooge-sort/
 typedef unsigned long u_long;
 
 /*
-    (T* A, unsigned long B, bool C, unsigned long D) -> ()
-    Takes an array A within range of D..B and sorts it in 
-    the ascending order if C - true and descending if C - false, 
-    using stooge sorting. 
-    - Changes the array given as argument, length and order remains
+	(T* A, unsigned long B, bool C) -> ()
+	Takes an array A with a lenght of B and sorts it in
+	the ascending order if C - true and descending if C - false,
+	using stooge sorting.
+	- Changes the array given as argument, length and order remains
 	unchanged.
 */
 template <typename T>
-void StoogeSort(T* array, u_long h, bool order = true, u_long l = 0)
+static void StoogeSort(T* array, u_long length, bool order = true)
 {
-        if (l >= h)
-            return;
+	length--;
+	StoogeSortRanged(array, 0, length, order);
+}
 
-        if (order == true) {
-            if (array[l] > array[h])
-                std::swap(array[l], array[h]);
-        }
-        if (order == false) {
-            if (array[l] < array[h])
-                std::swap(array[l], array[h]);
-        }
+/*
+	(T* A, unsigned long B, unsigned long C, bool D) -> ()
+	Takes an array A within range of B..C and sorts it in
+	the ascending order if D - true and descending if C - false,
+	using stooge sorting.
+	- Changes the array given as argument, length and order remains
+	unchanged.
+*/
+template <typename T>
+static void StoogeSortRanged(T* array, u_long start_point, u_long end_point, bool order)
+{
+	if (start_point >= end_point)
+		return;
 
-        if (h - l + 1 > 2) {
-            u_long t = (h - l + 1) / 3;
-            StoogeSort(array, h - t, order, l);
-            StoogeSort(array, h, order, l + t);
-            StoogeSort(array, h - t, order, l);
-        }
+	if (order == true) {
+		if (array[start_point] > array[end_point])
+			std::swap(array[start_point], array[end_point]);
+	}
+	if (order == false) {
+		if (array[start_point] < array[end_point])
+			std::swap(array[start_point], array[end_point]);
+	}
+
+	if (end_point - start_point + 1 > 2) {
+		u_long cut_index = (end_point - start_point + 1) / 3;
+		StoogeSortRanged(array, start_point, end_point - cut_index, order);
+		StoogeSortRanged(array, start_point + cut_index, end_point, order);
+		StoogeSortRanged(array, start_point, end_point - cut_index, order);
+	}
 }
